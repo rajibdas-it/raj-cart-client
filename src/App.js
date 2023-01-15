ed891fd24b8ea3fe2d61ad301ae79cb4cba9e8c1
rajibdas-it <rajibrad@gmail.com>
2023-01-16 01:07:32 +0600

    setup daisy ui

--- src/App.js
+++ src/App.js
@@ -3,7 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Raj-Cart E-Commerce</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
