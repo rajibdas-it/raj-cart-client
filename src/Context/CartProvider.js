a5498c5f40c9d0006b868e596cc770ffd819090b
rajibdas-it <rajibrad@gmail.com>
2023-01-23 02:13:20 +0600

    create increment/decrement button function

--- src/Context/CartProvider.js
+++ src/Context/CartProvider.js
@@ -23,8 +23,46 @@ const CartProvider = ({ children }) => {
     }
   };
 
-  const cartInfo = { handleAddToCart, cartItems };
-  console.log(cartItems);
+  const handleProductQuantity = (product, type) => {
+    if (type === "increment") {
+      const updatedCart = cartItems.map((item) => {
+        if (item._id === product._id) {
+          return { ...item, orderQty: item.orderQty + 1 };
+        }
+        return item;
+      });
+
+      setCartItems(updatedCart);
+    }
+    if (type === "decrement" && product.orderQty >= 2) {
+      const updatedCart = cartItems.map((item) => {
+        if (item._id === product._id) {
+          return { ...item, orderQty: item.orderQty - 1 };
+        }
+
+        return item;
+      });
+
+      setCartItems(updatedCart);
+    }
+
+    // if (product.orderQty === 0) {
+    //   const updateProd = cartItems.filter((item) => item._id !== product._id);
+    //   setCartItems(updateProd);
+    // }
+  };
+
+  const handleDeleteProduct = (product) => {
+    const updateProd = cartItems.filter((item) => item._id !== product._id);
+    setCartItems(updateProd);
+  };
+
+  const cartInfo = {
+    handleAddToCart,
+    cartItems,
+    handleProductQuantity,
+    handleDeleteProduct,
+  };
   return (
     <CartContext.Provider value={cartInfo}>{children}</CartContext.Provider>
   );
--- src/Pages/Cart/CartDetails.js
+++ src/Pages/Cart/CartDetails.js
@@ -1,7 +1,11 @@
 import React from "react";
+import { useContext } from "react";
 import { FaTrash } from "react-icons/fa";
+import { CartContext } from "../../Context/CartProvider";
 
 const CartDetails = ({ item }) => {
+  const { handleProductQuantity, handleDeleteProduct } =
+    useContext(CartContext);
   return (
     <div className="flex items-center justify-between gap-6 border border-gray-200 rounded p-4">
       <div className="w-28 flex-shrink-0">
@@ -14,20 +18,29 @@ const CartDetails = ({ item }) => {
       </div>
       {/* <div className="text-primary text-lg font-semibold">$960.00</div> */}
       <div className="flex border border-gray-300 text-gray-600 divide-x divide-gray-300 w-max">
-        <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">
+        <button
+          onClick={() => handleProductQuantity(item, "decrement")}
+          className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none"
+        >
           -
-        </div>
+        </button>
         <div className="h-8 w-8 text-sm flex items-center justify-center">
           {item.orderQty}
         </div>
-        <div className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none">
+        <button
+          onClick={() => handleProductQuantity(item, "increment")}
+          className="h-8 w-8 text-xl flex items-center justify-center cursor-pointer select-none"
+        >
           +
-        </div>
+        </button>
       </div>
       <div className="text-primary text-lg font-semibold">
         ${(item.price * item.orderQty).toFixed(2)}
       </div>
-      <button className="text-gray-600 cursor-pointer hover:text-primary">
+      <button
+        onClick={() => handleDeleteProduct(item)}
+        className="text-gray-600 cursor-pointer hover:text-primary"
+      >
         <FaTrash />
       </button>
     </div>
